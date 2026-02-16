8d78bf1cd33f1d1de2bd91d1aa4f5ca191be4abe
raquelroy <47817676+raquelroy@users.noreply.github.com>
2026-02-16 09:17:56 +0100

    writer + test

--- src/unidad1_project/writers/writers.py
+++ src/unidad1_project/writers/writers.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 from typing import Protocol
-from pandas import DataFrame
+import pandas as pd
 
 # TODO: writer a formato comprimido `parquet`
 # TODO: writer a formato comprimido `excel` ????
 # TODO: writer a `json`
 
 class Writer(Protocol):
-    def write(self, data_frame: DataFrame) -> None: ...
+    """
+
+    """
+    def write(self, data_frame: pd.DataFrame) -> None: ...
 
 class WriterCsv(Writer):
     def __init__(self, file_path: Path):
@@ -19,9 +22,35 @@ class WriterCsv(Writer):
         #       - ¿? revisar las columnas del archivo (numero, nombre...) ¿?
         self._file_path = file_path
 
-    def write(self, data_frame: DataFrame) -> None:
+
+
+    def _create_directory_if_not_exist(self) -> None:
+        self._file_path.parent.mkdir(parents=True, exist_ok=True)
+
+
+    def _validate_columns(self, df: pd.DataFrame) -> None:
+        existing_df = pd.read_csv(self._file_path, nrows=0)
+
+        if list(existing_df.columns) != list(df.columns):
+            raise ValueError(f"Column mismatch.\n"
+                    f"Existing: {list(existing_df.columns)}\n"
+                    f"New: {list(df.columns)}")
+
+    def write(self, data_frame: pd.DataFrame) -> None:
         print(f"writing file... {self._file_path}")
         print(data_frame)
 
+        self._create_directory_if_not_exist()
+
+        file_exists = self._file_path.exists()
+        if file_exists:
+            self._validate_columns(data_frame)
+
+        data_frame.to_csv(
+            self._file_path,
+            mode="a",
+            index=False,
+            header=not file_exists
+        )
         # TODO: logica de guardado
         # Abrir archivo en modo append y añadir el contenido del df
