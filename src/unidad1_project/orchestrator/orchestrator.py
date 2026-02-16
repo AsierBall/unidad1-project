480e417040e93c9e4ebb0eeff0bb78648bf05b66
Asier <asierolvega@gmail.com>
2026-02-16 11:14:53 +0100

    add documentation to the Orchestrator class

--- src/unidad1_project/orchestrator/orchestrator.py
+++ src/unidad1_project/orchestrator/orchestrator.py
@@ -5,12 +5,30 @@ from pathlib import Path
 from functools import reduce
 
 class Orchestrator():
+    """
+    Orchestrator that executes a set of `Transformer` to a
+    data stream coming from a `Reader` and writes it using `Writer`
+
+    :param reader: reader with the read configuration
+    :type reader: Reader
+    :param transformers: a list of transformers that must be executed to the data
+    :type transformers: list[Transformer]
+    :param writer: a writer with the write configuration
+    :type writer: Writer
+    """
     def __init__(self, reader: Reader, transformers: list[Transformer], writer: Writer):
         self._reader = reader
         self._transformers = transformers
         self._writer = writer
 
     def run(self, input_file_path: Path, output_file_path: Path):
+        """runs the data stream from reading to writing
+
+        :param input_file_path: path to the input file
+        :type input_file_path: Path
+        :param output_file_path: path to the output file
+        :type output_file_path: Path
+        """
         for chunk in self._reader.read(input_file_path):
             cleaned_chunk = reduce(
                 lambda data, transformer: transformer.transform(data),
